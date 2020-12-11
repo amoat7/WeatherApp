fdbcfb87caac1dfa7685687c111f61c367aac1d5
David Amoateng <amoat7@gmail.com>
2020-12-11 14:07:55 +1000

    font change

--- src/Containers/WeatherBuilder/WeatherBuilder.js
+++ src/Containers/WeatherBuilder/WeatherBuilder.js
@@ -4,6 +4,7 @@ import classes from './WeatherBuilder.module.css'
 import axios from 'axios'
 
 
+
 class weatherBuilder extends Component{
 
     state ={
@@ -14,9 +15,29 @@ class weatherBuilder extends Component{
         icon: '',
         temp: '',
         weather:'',
+        tempcolor: '',
         
 
-        locations :[]
+        locations :[],
+        content :[]
+    }
+
+    deleteLocation=(id)=>{
+
+        const locations = this.state.locations.filter(location=>{
+            return location.id!==id
+        })
+
+        this.setState({locations:locations})
+        const content = locations.map(location=>{
+            return(
+                location.content
+            )
+        })
+        
+        this.setState({content:content})
+        
+
     }
 
     async componentDidUpdate(){
@@ -33,6 +54,20 @@ class weatherBuilder extends Component{
                 const country = res.data.sys.country
                 const conditions = res.data.weather[0].description
                 const temp = (parseFloat(res.data.main.temp) - 273.15)
+                if(temp>30){
+                    const tempcolor = 'red'
+                    this.setState({tempcolor})
+                }
+
+                else if(temp<15){
+                    const tempcolor = 'blue'
+                    this.setState({tempcolor})
+                }
+
+                else{
+                    const tempcolor = 'green'
+                    this.setState({tempcolor})
+                }
                 console.log(res)     
 
                 const icon = `http://openweathermap.org/img/wn/${res.data.weather[0].icon}.png`;
@@ -45,23 +80,42 @@ class weatherBuilder extends Component{
                 this.setState({temp: temp.toFixed(1)})
                 this.setState({weather: conditions})
 
-                const location = 
-                            <div key={this.state.id} className={classes.TopLevel}>
-                                <div  className={classes.Weather}>
-                            <h1 >{this.state.cityName}, {this.state.country}</h1>
-                            <div>
-                                <img style={{width: '40%'}} src={this.state.icon} alt="WeatherIcon"/>
-                            </div>
-                            <h2>{this.state.weather}</h2>
-                            <h2>{this.state.temp}<span>&#176;</span>C  </h2>
-                           
-                            <button>Forecast</button>
-                        </div>
-                            </div>
+
+                
+
+                const idx = this.state.id;
+
+                const location = {};
+                location.id = this.state.id
+                location.content = 
+                        
+                    <div key={this.state.id} className={classes.TopLevel}>
                             
-                console.log(location.props.children.props.children[0].props.children[0])
+                            <div  className={classes.Weather}>
+                            <span className={classes.Delete} onClick={()=>{this.deleteLocation(idx)}}>X</span>
+                        <h1 >{this.state.cityName}, {this.state.country}</h1>
+                        <div>
+                            <img style={{width: '40%'}} src={this.state.icon} alt="WeatherIcon"/>
+                        </div>
+                        <h2>{this.state.weather}</h2>
+                        <h2 style={{color: this.state.tempcolor}}>{this.state.temp}<span>&#176;</span>C  </h2>
+                        
+                        <button>Forecast</button>
+                        </div>
+                    </div>
+                                                
+                //console.log(location.props.children.props.children[0].props.children[0])
                 const locations = [ location, ...this.state.locations]
                 this.setState({locations: locations})
+
+                const content = locations.map(location=>{
+                    return(
+                        location.content
+                    )
+                })
+                
+                this.setState({content:content})
+                
                 
 
 
@@ -76,7 +130,7 @@ class weatherBuilder extends Component{
 
             return(
                 <div className={classes.Locations}>
-                    {this.state.locations}
+                    {this.state.content}
                 </div>            
     
             )
